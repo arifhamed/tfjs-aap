0567d6db64ade7506e13f2b10c8f6c06cfe66b96
arifhamed <arifhamed2002@gmail.com>
2023-01-25 01:02:44 +0800

    25-Jan-2023 01:02:43:  (from arifhamed.github.io)

--- st-news-ai/deployment.js
+++ st-news-ai/deployment.js
@@ -3,12 +3,12 @@
 console.log("mike test 1 2 3..");
 function httpGet(theUrl)
 {
-    // if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-    //     xmlhttp=new XMLHttpRequest();
-    // } else {// code for IE6, IE5
-    //     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    // }
-    xmlhttp=new XMLHttpRequest();
+    if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    } else {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    // xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
         if (xmlhttp.readyState==4 && xmlhttp.status==200){
             document.getElementById("news_headline_1").innerHTML = xmlhttp.responseText;
@@ -17,4 +17,5 @@ function httpGet(theUrl)
     xmlhttp.open("GET", theUrl, false);
     xmlhttp.send();
 }
+httpGet("https://straitstimes.com/sitemap.xml")
 // document.getElementById("news_headline_1").innerHTML = await httpGet("https://straitstimes.com/sitemap.xml")
