d10469b692bdebbf6aae78d448cea8ea3d79b92a
arifhamed <arifhamed2002@gmail.com>
2023-01-25 12:26:48 +0800

    25-Jan-2023 12:26:48:  (from arifhamed.github.io)

--- st-news-ai/deployment.js
+++ st-news-ai/deployment.js
@@ -50,9 +50,10 @@ newsFace();
 //     return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
 // }
 async function newsFace() {
-    const response = await fetch("https://newsapi.org/v2/top-headlines?country=sg&apiKey=9f2eaa817499484aa81b95de9b708027");
+    // for some reason sg doesn't work but my works :/
+    const response = await fetch("http://api.mediastack.com/v1/news?access_key=a94c1c74b0ba73efb9d82b4a66c05a67&countries=sg,my&languages=en");
     const all = await response.json();
-    const current = all['articles'][0]["title"];
+    const current = all['data'][0]["title"];
     // const bruh = convertTZ(current.substring(0,4)+"/"+current.substring(5,7)+"/"+current.substring(8,10)+" "+current.substring(11,14)+":"+current.substring(14,17)+":"+current.substring(17,19)+" +0000", "Asia/Singapore");
     // const zeroPad = (num, places) => String(num).padStart(places, '0');
     document.querySelector("#news_headline_1").innerHTML = current
